b7a91bf88b3584d8d592391c3a194dfc69b78a5a
GuillianV <guillianvibert90@gmail.com>
2026-01-25 21:39:46 +0100

    feat: property class

--- modules/download-assets.js
+++ modules/download-assets.js
@@ -18,37 +18,37 @@ if (!fs.existsSync(datafolder)) fs.mkdirSync(datafolder);
 
 console.log("Downloading game files...");
 
-// const child = spawn("modules/downloader/hytale-downloader-linux-amd64", [
-//   "-download-path",
-//   downloadfolder + "/" + downloadfile,
-// ]);
+const child = spawn("modules/downloader/hytale-downloader-linux-amd64", [
+  "-download-path",
+  downloadfolder + "/" + downloadfile,
+]);
 
-// child.stdout.on("data", (chunk) => {
-//   console.log(chunk.toString());
-// });
+child.stdout.on("data", (chunk) => {
+  console.log(chunk.toString());
+});
 
-// child.on("close", (code) => {
-//   if (code === 0) {
-//     extractZip().then(() => {
-//       console.log("Downloading files complete");
-//       process.exit(0);
-//     });
-//   } else {
-//     console.log("Download failed");
-//   }
-// });
+child.on("close", (code) => {
+  if (code === 0) {
+    extractZip().then(() => {
+      console.log("Downloading files complete");
+      process.exit(0);
+    });
+  } else {
+    console.log("Download failed");
+  }
+});
 
 async function extractZip() {
   return new Promise(async (resolve, reject) => {
-    // await extract(
-    //   path.join(__rootname, downloadfolder, downloadfile),
-    //   { dir: path.join(__rootname, downloadfolder, extractedname) },
-    //   (err) => {
-    //     if (err) {
-    //       reject(err);
-    //     }
-    //   },
-    // );
+    await extract(
+      path.join(__rootname, downloadfolder, downloadfile),
+      { dir: path.join(__rootname, downloadfolder, extractedname) },
+      (err) => {
+        if (err) {
+          reject(err);
+        }
+      },
+    );
 
     const assteszip = path.join(
       __rootname,
